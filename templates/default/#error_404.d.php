5f7ecb35f5ba052f3e53bd7f23650c7289d5d5cd
konserv <a@konservs.com>
2017-03-14 17:51:48 +0200

    404 error

--- templates/default/#error_404.d.php
+++ templates/default/#error_404.d.php
@@ -5,17 +5,38 @@
  * @author Andrii Biriev
  */
 defined('BEXEC')or die('No direct access!');
+use Application\BRouter;
 use Brilliant\cms\BLang;
+use Brilliant\html\BHTML;
+
+$bhtml=BHTML::getInstance();
+$bhtml->add_meta('viewport','width=device-width, initial-scale=0.35, maximum-scale=1, user-scalable=yes');
+$brouter=BRouter::getInstance();
+$bhtml->use_framework('bootstrap');
+
+$bhtml->load_css(dirname(__FILE__).DIRECTORY_SEPARATOR.'css'.DIRECTORY_SEPARATOR.'#error_404.d.css');
 ?><!DOCTYPE html>
 <html>
 <head>
 	<title><?php echo BLang::_("404_PAGE_NOT_FOUND"); ?></title>
 	<link rel=stylesheet href="<?php echo '//'.BHOSTNAME_STATIC.'/css/errors.css'; ?>" type="text/css" />
+	<?php echo $bhtml->out_head();?>
 </head>
 
 <body>
-	<div class="NotFound">
-		<h1>404</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="error-template">
+					<h1>Oops!</h1>
+					<h2>404 Not Found</h2>
+					<div class="error-details">Sorry, an error has occured, Requested page not found!</div>
+					<div class="error-actions">
+						<a href="/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span> Take Me Home </a>
+						<a href="/" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-envelope"></span> Contact Support </a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-</body>
-</html>
+</body></html>
