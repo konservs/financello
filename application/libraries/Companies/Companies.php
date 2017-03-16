b0b8e4dc3d842e4ee0e631ddc3dc6ba1894fbae6
konserv <a@konservs.com>
2017-03-16 19:30:38 +0200

    Added libraries dummy

---
+++ application/libraries/Companies/Companies.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Basic class to control companies.
+ *
+ * @author Andrii Biriev
+ */
+namespace Application\Companies;
+
+class Companies extends \Brilliant\Items\BItemsList{
+	use \Brilliant\BSingleton;
+	}
---
+++ application/libraries/Companies/Company.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Basic class to control companies.
+ *
+ * @author Andrii Biriev
+ */
+namespace Application\Companies;
+
+class Company extends \Brilliant\Items\BItemsItem{
+
+	}
