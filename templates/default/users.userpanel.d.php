46c646f740b17e6db96c9f19fa8afe8a8548ee41
konserv <a@konservs.com>
2017-03-21 14:12:30 +0200

    Userpanel URLs fixed

--- templates/default/users.userpanel.d.php
+++ templates/default/users.userpanel.d.php
@@ -11,12 +11,12 @@ $bRouter = BRouter::getInstance();
 ?>
 <ul class="nav navbar-nav navbar-right">
 	<?php if(empty($this->user)): ?>
-		<?php $urlLogin = '//'.$bRouter->generateURL('users',array('view'=>'login','lang'=>BLang::$langcode)); ?>
-		<?php $urlRegister = '//'.$bRouter->generateURL('users',array('view'=>'register','lang'=>BLang::$langcode)); ?>
+		<?php $urlLogin = $bRouter->generateURL('users',array('view'=>'login','lang'=>BLang::$langcode)); ?>
+		<?php $urlRegister = $bRouter->generateURL('users',array('view'=>'register','lang'=>BLang::$langcode)); ?>
 		<li><a href="<?php echo $urlLogin; ?>"><?php echo BLang::_('USERS_LOGIN'); ?></a></li>
 		<li><a href="<?php echo $urlRegister; ?>"><?php echo BLang::_('USERS_REGISTER'); ?></a></li>
 	<?php else: ?>
-		<?php $urlLogout = '//'.$bRouter->generateURL('users',array('view'=>'logout','lang'=>BLang::$langcode)); ?>
+		<?php $urlLogout = $bRouter->generateURL('users',array('view'=>'logout','lang'=>BLang::$langcode)); ?>
 		<li><a href="<?php echo $urlLogout; ?>"><?php echo htmlspecialchars($this->user->getname(20)); ?></a></li>
 		<li><a href="<?php echo $urlLogout; ?>"><?php echo BLang::_('USERS_LOGOUT'); ?></a></li>
 	<?php endif; ?>
