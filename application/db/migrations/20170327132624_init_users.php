7aefefb7e11f5ddd60f727309fb6d7ddc9256816
konserv <a@konservs.com>
2017-03-27 16:30:21 +0300

    Installed Phinx

---
+++ application/db/migrations/20170327132624_init_users.php
@@ -0,0 +1,13 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class InitUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     */
+    public function change()
+    {
+
+    }
+}
