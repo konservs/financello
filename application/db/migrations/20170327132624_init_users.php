23380ead76240778a3350c01ec729cb505fdffe0
konserv <a@konservs.com>
2017-04-01 17:36:23 +0300

    Users migration

--- application/db/migrations/20170327132624_init_users.php
+++ application/db/migrations/20170327132624_init_users.php
@@ -1,13 +1,24 @@
 <?php
 use Phinx\Migration\AbstractMigration;
 
-class InitUsers extends AbstractMigration
-{
-    /**
-     * Change Method.
-     */
-    public function change()
-    {
-
-    }
-}
+class InitUsers extends AbstractMigration{
+	/**
+	 *
+	 */
+	public function change(){
+		$table = $this->table('users');
+		$table->addColumn('email', 'string')
+			->addColumn('status', 'enum', ['values'=>['P','N','D'],'default'=>'N'])
+			->addColumn('isadmin', 'enum', ['values'=>['Y','N'],'default'=>'N'])
+			->addColumn('password', 'char', ['length'=>128])
+			->addColumn('name', 'string')
+			->addColumn('firstname', 'string')
+			->addColumn('lastname', 'string')
+			->addColumn('middlename', 'string')
+			->addColumn('created', 'datetime')
+			->addColumn('modified', 'datetime')
+			->addColumn('last_action', 'datetime',['null'=>true])
+			->addIndex(['email'], ['unique' => true])
+			->create();
+		}
+	}
