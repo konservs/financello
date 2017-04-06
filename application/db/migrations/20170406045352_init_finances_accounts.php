c6b3415a48511327a01fd41312e4a80906c73395
konserv <a@konservs.com>
2017-04-06 07:57:48 +0300

    Fixed CompaniesCurrencies, added Accounts migration

--- application/db/migrations/20170406045352_init_finances_accounts.php
+++ application/db/migrations/20170406045352_init_finances_accounts.php
@@ -3,17 +3,21 @@
 use Phinx\Migration\AbstractMigration;
 use Phinx\Db\Adapter\MysqlAdapter;
 
-class InitFinancesCompanyCurrencies extends AbstractMigration{
+class InitFinancesAccounts extends AbstractMigration{
 	/**
 	 *
 	 */
 	public function change(){
-		$table = $this->table('fin_ccurrencies',array('id' => false, 'primary_key' => array('id')));
+		$table = $this->table('fin_accounts',array('id' => false, 'primary_key' => array('id')));
 		$table->addColumn('id', 'integer', ['signed'=>false, 'identity'=>true])
 			->addColumn('company', 'integer', ['signed'=>false])
 			->addColumn('currency', 'integer', ['signed'=>false,'limit' => MysqlAdapter::INT_SMALL])
-			->addColumn('flags', 'string')
+			->addColumn('name', 'string', ['length'=>100])
+			->addColumn('icon', 'string', ['length'=>10])
 			->addColumn('balance', 'decimal', ['scale'=>2, 'precision'=>20, 'default'=>0])
+			->addColumn('limit', 'decimal', ['scale'=>2, 'precision'=>20, 'default'=>0])
+			->addColumn('created', 'datetime', ['default' => 'CURRENT_TIMESTAMP'])
+			->addColumn('modified', 'datetime', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
 			->addForeignKey('company', 'companies', 'id', array('delete'=> 'CASCADE', 'update'=> 'CASCADE'))
 			->addForeignKey('currency', 'fin_currencies', 'id', array('delete'=> 'RESTRICT', 'update'=> 'CASCADE'))
 			->create();
