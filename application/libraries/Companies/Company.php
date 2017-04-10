02b26b07ab66df97eb14c8c705d2d946941d400d
konserv <a@konservs.com>
2017-04-10 13:30:03 +0300

    Documented Company class

--- application/libraries/Companies/Company.php
+++ application/libraries/Companies/Company.php
@@ -8,9 +8,9 @@ namespace Application\Companies;
 
 use Application\Companies\Company;
 
-class Company extends \Brilliant\Items\BItemsItem{
-	protected $collectionname='Company';
-	protected $tablename='companies';
+class Company extends \Brilliant\Items\BItemsItem {
+	protected $collectionName = 'Company';
+	protected $tableName = 'companies';
 	/**
 	 * @var DateTime
 	 */
@@ -20,24 +20,42 @@ class Company extends \Brilliant\Items\BItemsItem{
 	 */
 	public $modified;
 	/**
+	 * @var string
+	 */
+	public $name;
+	/**
+	 * @var string
+	 */
+	public $status;
+	/**
+	 * @var string
+	 */
+	public $published;
+	/**
+	 * @var int
+	 */
+	public $director;
+
+	/**
 	 * Constructor - init fields...
 	 */
 	function __construct() {
 		parent::__construct();
-		$this->fieldAddRaw('status','enum',array('values'=>array('O','C')));//Opened or closed
-		$this->fieldAddRaw('published','enum',array('values'=>array('P','N','D')));
-		$this->fieldAddRaw('name','string');
-		$this->fieldAddRaw('director','int');
+		$this->fieldAddRaw('status', 'enum', array('values' => array('O', 'C')));//Opened or closed
+		$this->fieldAddRaw('published', 'enum', array('values' => array('P', 'N', 'D')));
+		$this->fieldAddRaw('name', 'string');
+		$this->fieldAddRaw('director', 'int');
 		//Statistics (all fields are readonly)
-		$this->fieldAddRaw('counter_users','int',array('readonly'=>true));
+		$this->fieldAddRaw('counter_users', 'int', array('readonly' => true));
 		//Created & modified
-		$this->fieldAddRaw('created','dt',array('readonly'=>true));
-		$this->fieldAddRaw('modified','dt',array('readonly'=>true));
-		}
+		$this->fieldAddRaw('created', 'dt', array('readonly' => true));
+		$this->fieldAddRaw('modified', 'dt', array('readonly' => true));
+	}
+
 	/**
 	 *
 	 */
-	public function canUser($userId,$flag){
+	public function canUser($userId, $flag) {
 		return true;
-		}
 	}
+}
