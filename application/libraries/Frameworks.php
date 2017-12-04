17c15f938cda1432a41d1cfad4fc4a29ae2376fc
konserv <a@konservs.com>
2017-12-04 14:44:12 +0200

    Moved CSS/JS libraries to composer

--- application/libraries/Frameworks.php
+++ application/libraries/Frameworks.php
@@ -22,15 +22,16 @@ class Frameworks{
 		foreach($this->list as $framework){
 			switch($framework){
 				case 'jquery': 
-					$html->add_js('//'.BHOSTNAME_STATIC.'/libs/jquery/2.1.3/jquery-2.1.3.min.js','',JS_PRIORITY_FRAMEWORK);
+					$html->add_js('//'.BHOSTNAME_STATIC.'/assets/jquery/jquery.min.js','',JS_PRIORITY_FRAMEWORK);
 					break;
 				case 'bootstrap':
-					$html->add_js('//'.BHOSTNAME_STATIC.'/libs/bootstrap/3.3.4/js/bootstrap.min.js','',JS_PRIORITY_FRAMEWORK2);//after jQuery
-					$html->add_css('//'.BHOSTNAME_STATIC.'/libs/bootstrap/3.3.4/css/bootstrap.min.css','',CSS_PRIORITY_FRAMEWORK);
-					$html->add_css('//'.BHOSTNAME_STATIC.'/libs/bootstrap/3.3.4/css/bootstrap-theme.min.css','',CSS_PRIORITY_FRAMEWORK2);
+					$html->add_js('//'.BHOSTNAME_STATIC.'/assets/bootstrap/js/bootstrap.min.js','',JS_PRIORITY_FRAMEWORK2);//after jQuery
+					$html->add_css('//'.BHOSTNAME_STATIC.'/assets/bootstrap/css/bootstrap.min.css','',CSS_PRIORITY_FRAMEWORK);
+					$html->add_css('//'.BHOSTNAME_STATIC.'/assets/bootstrap/css/bootstrap-theme.min.css','',CSS_PRIORITY_FRAMEWORK2);
 					break;
 				case 'select2': 
-					$html->add_js('//'.BHOSTNAME_STATIC.'/libs/select2/select2.min.js');
+					$html->add_js('//'.BHOSTNAME_STATIC.'/assets/select2/select2-built.js');
+					$html->add_css('//'.BHOSTNAME_STATIC.'/assets/select2/select2-built.css');
 					break;					
 				case 'font-awesome':
 					$html->add_css('//'.BHOSTNAME_STATIC.'/fonts/font-awesome/4.7.0/css/font-awesome.min.css','',CSS_PRIORITY_FRAMEWORK2);
