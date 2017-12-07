3ea5d213dff53ce0b7e5b97eccce44d593ff1b07
konserv <a@konservs.com>
2017-12-07 09:49:40 +0200

    Migrated into Select2 v.4.0

--- static/js/select2_filters/fin_currencies.js
+++ static/js/select2_filters/fin_currencies.js
@@ -1,41 +1,39 @@
-$(document).ready(function(){
-	$.fn.select2.amd.require(['select2/data/array','select2/utils'], function (ArrayData, Utils) {
-	function CustomDataFinCurrencies ($element, options) {
-		CustomDataFinCurrencies.__super__.constructor.call(this, $element, options);
+/**
+ *
+ * @param text
+ * @returns {XML|*|string|void}
+ */
+function escapeHtml(text) {
+	if(text==undefined){
+		return '';
 		}
-	Utils.Extend(CustomDataFinCurrencies, ArrayData);
-	CustomDataFinCurrencies.prototype.current = function (callback) {
-		var data = [];
-		var currentVal = this.$element.val();
-		if (!this.$element.prop('multiple')) {
-			currentVal = [currentVal];
-			}
-		for (var v = 0; v < currentVal.length; v++) {
-			data.push({id: currentVal[v],text: currentVal[v]});
-			}
-		callback(data);
+	var map = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#039;'
 		};
+	return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+	}
 
+$(document).ready(function(){
 	var $element = $(".select2fincurrencieslist").select2({
-		dataAdapter: CustomDataFinCurrencies
-		});
-	var $request = $.ajax({
-		url: window.urls.select2currenciesfilter
-		});
-	$request.then(function (data) {
-		// This assumes that the data comes back as an array of data objects
-		// The idea is that you are using the same callback as the old `initSelection`
-		for (var d = 0; d < data.length; d++) {
-			var item = data[d];
-			// Create the DOM option that is pre-selected by default
-			var option = new Option(item.text, item.id, true, true);
-			// Append it to the select
-			$element.append(option);
+		ajax: {
+			url: window.urls.select2currenciesfilter,
+			dataType: 'json',
+			processResults: function (data) {
+				pg = {};
+				pg.more = true;
+				return {results: data.currencies, pagination:pg};
+				}
+			},
+		templateResult: function (currency){
+			return escapeHtml(currency.name)+ ' <b>' + currency.code3 +'</b> (#'+currency.id+')';
+			},
+		templateSelection: function (currency){
+			return escapeHtml(currency.name)+ ' <b>' + currency.code3 +'</b> (#'+currency.id+')';
 			}
-		// Update the selected options that are displayed
-		$element.trigger('change');
 		});
-	//
 	});
-});
 
