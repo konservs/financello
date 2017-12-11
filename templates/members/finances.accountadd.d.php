ac2ae2ab282adad85bd57b5843c3c0fd17b5a08c
konserv <a@konservs.com>
2017-12-11 14:39:03 +0200

    Add account form

--- templates/members/finances.accountadd.d.php
+++ templates/members/finances.accountadd.d.php
@@ -6,6 +6,8 @@
  */
 defined('BEXEC') or die('No direct access!');
 use \Brilliant\CMS\BLang;
+$brouter=\Application\BRouter::getInstance();
+$urlAccounts = $brouter->generateURL('finances',array('view'=>'accounts','company'=>$this->company->id));
 //
 $js='';
 $js.='window.urls={};'.PHP_EOL;
@@ -22,11 +24,55 @@ $this->useFramework('select2-finances-currencies');
 
 	<?php $this->breadcrumbs->draw(); ?>
 
-	<div class="panel-body row">
+
+	<div class="row">
 		<div class="col-lg-3 col-md-12 col-sm-12 opgname form-group">
-			<label class="sr-only" for="<?php echo 'fincurrencieslist'; ?>">Currency</label>
+			<label for="<?php echo 'fin_account_name'; ?>">Name</label>
+		</div>
+		<div class="col-lg-9 col-md-12 col-sm-12 opgname form-group">
+			<input type="text" id="<?php echo 'fin_account_name'; ?>" class="form-control input-sm" name="name" placeholder="Account name, for example, Credit Card"/>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-3 col-md-12 col-sm-12 opgname form-group">
+			<label for="<?php echo 'fincurrencieslist'; ?>">Currency</label>
+		</div>
+		<div class="col-lg-9 col-md-12 col-sm-12 opgname form-group">
 			<select id="<?php echo 'fincurrencieslist'; ?>" class="form-control input-sm select2fincurrencieslist" name="currency" placeholder="Currency"></select>
 		</div>
 	</div>
+
+
+	<div class="row">
+		<div class="col-lg-3 col-md-12 col-sm-12 opgname form-group">
+			<label for="<?php echo 'fin_account_icon'; ?>">Icon</label>
+		</div>
+		<div class="col-lg-9 col-md-12 col-sm-12 opgname form-group">
+			<select id="<?php echo 'fin_account_icon'; ?>" class="form-control input-sm" name="icon" placeholder="Icon">
+				<option value="A">Icon A</option>
+				<option value="B">Icon B</option>
+			</select>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-3 col-md-12 col-sm-12 opgname form-group">
+			<label for="<?php echo 'fin_account_limit'; ?>">Credit Limit</label>
+		</div>
+		<div class="col-lg-9 col-md-12 col-sm-12 opgname form-group">
+			<input type="text" id="<?php echo 'fin_account_limit'; ?>" class="form-control input-sm" name="limit" value="0"/>
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="col-lg-3 col-md-12 col-sm-12 opgname form-group">
+		</div>
+		<div class="col-lg-9 col-md-12 col-sm-12 opgname form-group">
+			<button class="btn btn-default" role="submit">Save</button>
+			<a class="btn btn-default" href="<?php echo $urlAccounts; ?>">Cancel</a>
+		</div>
+	</div>
+
 
 </div>
